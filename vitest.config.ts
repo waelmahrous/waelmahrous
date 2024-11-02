38ffe3b3f0f3328b1ae824b9980cf08caae26c92
Wael Mahrous <waelbmahrous@gmail.com>
2024-11-02 22:32:28 +0100

    Added testing and coverage gates in gh actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,16 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     test: {
         coverage: {
-            include: ['/src/*'],
+            include: ['src/components/**.{ts,tsx}'],
+            reporter: ['text'],
+            thresholds: {
+                global: {
+                    branches: 80,
+                    functions: 80,
+                    lines: 80,
+                    statements: 80,
+                },
+            }
         },
     },
 })
