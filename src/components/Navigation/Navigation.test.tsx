87ceb20399ac9e600a056180162bcae84ca927d7
Wael Mahrous <waelbmahrous@gmail.com>
2024-11-06 19:35:33 +0100

    Added navigation component and test suite

---
+++ src/components/Navigation/Navigation.test.tsx
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe, test } from "vitest";
+
+describe('Navigation', () => {
+    /**
+     * Navigation test list
+     * - Should render links to all available pages
+     * - Pressing link changes the element to be rendered
+     */
+
+    
+})
