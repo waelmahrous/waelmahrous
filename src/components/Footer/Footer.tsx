edd06e5ec07be94bc6d8e6afa1c83b7d8b86f22d
Wael Mahrous <waelbmahrous@gmail.com>
2024-11-09 17:22:16 +0100

    Only show footer when in Home

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,9 @@
 import { Box, Typography } from '@mui/material'
+import { useContext } from 'react'
+import { PageContext } from '../Pages/PageProvider'
 
 function Footer() {
+    const { page } = useContext(PageContext)!
     return (
         <Box sx={{
             height: "100%",
@@ -10,9 +13,10 @@ function Footer() {
             alignItems: "center",
             justifyContent: "flex-end"
         }}>
-            <Typography variant="body1" gutterBottom>
-                Made with React!
-            </Typography>
+            {page?.key == "Home" &&
+                <Typography variant="body1" gutterBottom>
+                    Made with React!
+                </Typography>}
         </Box>
     )
 }
