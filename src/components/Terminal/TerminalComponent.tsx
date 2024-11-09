5942ee2965febefde3956f7acf65f2b2259ce4f9
Wael Mahrous <waelbmahrous@gmail.com>
2024-11-09 17:20:45 +0100

    Terminal upgrades
    This took longer than I liked. But this is now a functioning terminal
    component that connects to a local gotty socket.

--- src/components/Terminal/TerminalComponent.tsx
+++ src/components/Terminal/TerminalComponent.tsx
@@ -1,48 +1,71 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Hterm } from "../../../gotty/js/src/hterm";
 import { WebTTY, protocols } from "../../../gotty/js/src/webtty";
 import { ConnectionFactory } from "../../../gotty/js/src/websocket";
 import { Box } from "@mui/material";
+import { defaultColors } from "../../theme/theme";
 
 const TerminalComponent = () => {
+    const [ready, setReady] = useState(false)
 
     useEffect(() => {
-        // Get the terminal element after component has mounted
         const elem = document.getElementById("terminal");
 
         if (elem !== null) {
-            // Initialize the terminal
             var term = new Hterm(elem);
 
-            // Set the WebSocket URL for Gotty running locally (on port 8080)
-            const url = "ws://localhost:8080/ws";  // Replace with your Gotty WebSocket URL
-            const args = "";  // Any additional URL parameters for Gotty can be included here if needed
+            const url = "ws://localhost:8080/ws";
+            const args = "";
             const factory = new ConnectionFactory(url, protocols);
 
-            // Create WebTTY connection
-            const wt = new WebTTY(term, factory, args, "someauth");
+            const wt = new WebTTY(term, factory, args, "user:password");
             const closer = wt.open();
 
-            // Close the terminal when the page is unloaded
+            term.term.prefs_.set('font-family', '"Courier Bold", "Lucida Console", "Courier New", "Roboto Mono", monospace');
+            term.term.prefs_.set('font-size', 20);
+            term.term.prefs_.set('foreground-color', '#33FF33');
+            term.term.prefs_.set('background-color', defaultColors.background);
+            term.term.prefs_.set('font-smoothing', 'antialiased');
+            term.term.prefs_.set('scroll-on-output', false);
+            term.term.prefs_.set('scroll-on-keystroke', false);
+            term.term.prefs_.set('scrollbar-visible', false);
+
+            const randomInterval = () => {
+                return Math.random() * (3000 - 500) + 500;
+            };
+
+            const sendRandomKeystrokes = () => {
+                term.io.sendString("!")
+
+                setTimeout(sendRandomKeystrokes, randomInterval());
+            };
+
             window.addEventListener("unload", () => {
                 closer();
                 term.close();
             });
 
-            // Cleanup the WebTTY connection when the component is unmounted
+            sendRandomKeystrokes();
+            term.term.scrollOnKeystroke_ = false
+
+            term.term.onTerminalReady = () => {
+                setReady(true)
+            }
+
             return () => {
                 closer();
                 term.close();
             };
         }
-    }, []); // Empty dependency array ensures this effect runs only once, when the component mounts
+    }, []);
 
     return (
-        // <div id="terminal" sxstyle="position:relative; width:100%; height:100%"></div>
-        <div id="terminal" style={{
+        <Box flex={1} id="terminal" sx={{
             position: "relative",
             height: "100%",
-            width: "100%"
+            width: "100%",
+
+            opacity: ready ? "100%" : "0%"
         }} />
     );
 };
